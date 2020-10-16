60351f1ccba89fc73fadbb4f829c9a384e27b81a
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-10-16 16:32:55 +0100

    explosion bullet

--- Game Engines Examples/Assets/TankSink.cs
+++ Game Engines Examples/Assets/TankSink.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TankSink : MonoBehaviour
 {
+    private float radius = 0.5f;
+
     private void OnEnable()
     {
         StartCoroutine(Sink());
@@ -17,5 +19,20 @@ public class TankSink : MonoBehaviour
         GetComponent<Rigidbody>().drag = 1;
         yield return new WaitForSeconds(3);
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            if(transform.childCount > 0)
+            {
+                transform.GetChild(0).gameObject.AddComponent<Rigidbody>();
+                transform.GetChild(0).gameObject.AddComponent<Cleanup>();
+                transform.DetachChildren();
+            }
+            Vector3 pos = new Vector3(Random.Range(-radius, radius), transform.position.y, Random.Range(-radius, radius));
+            GetComponent<Rigidbody>().AddExplosionForce(15f, pos, 10f);
+        }
     }
 }
