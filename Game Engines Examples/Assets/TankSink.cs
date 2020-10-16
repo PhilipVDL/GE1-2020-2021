15c539f2aa9cf98a0ab80c9b7c53527d0d1a1b46
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-10-16 15:51:28 +0100

    lab 4: sinking tanks

---
+++ Game Engines Examples/Assets/TankSink.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSink : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(Sink());
+    }
+
+    IEnumerator Sink()
+    {
+        yield return new WaitForSeconds(4);
+        GetComponent<Collider>().enabled = false;
+        transform.GetChild(0).GetComponent<Collider>().enabled = false;
+        GetComponent<Rigidbody>().drag = 1;
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
---
+++ Game Engines Examples/Assets/TankSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawn : MonoBehaviour
+{
+    public GameObject tank;
+    public float radius;
+    public float spawnDelay;
+    public int maxSpawns;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnCoroutine(spawnDelay));
+    }
+
+    void Spawn()
+    {
+        Vector3 pos = new Vector3(Random.Range(-radius, radius), 3, Random.Range(-radius, radius));
+        GameObject enemy = Instantiate(tank, pos, Quaternion.identity);
+        enemy.transform.SetParent(transform);
+    }
+
+    IEnumerator SpawnCoroutine(float delay)
+    {
+        while (true)
+        {
+            if(transform.childCount < maxSpawns)
+            {
+                Spawn();
+            }
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
