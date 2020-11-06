66dd48edb72c81e2f1fac27c00bc4be20273cf42
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-11-06 15:44:50 +0000

    comments excercise

--- Game Engines Examples 2020/Assets/SpineAnimator.cs
+++ Game Engines Examples 2020/Assets/SpineAnimator.cs
@@ -10,18 +10,18 @@ public class SpineAnimator : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        Cursor.visible = false;
+        Cursor.visible = false; //hides cursor
         for (int i = 0; i < transform.childCount; i++)
         {
-            Transform current = transform.GetChild(i);
+            Transform current = transform.GetChild(i); //tracks transform of current object
             if (i > 0)
             {
-                Transform prev = transform.GetChild(i - 1);
-                Vector3 offset = current.transform.position - prev.transform.position; 
-                offset = Quaternion.Inverse(prev.transform.rotation) * offset;
-                offsets.Add(offset);                
+                Transform prev = transform.GetChild(i - 1); //tracks transform of object just before this object
+                Vector3 offset = current.transform.position - prev.transform.position; //calculates distance between the two objects
+                offset = Quaternion.Inverse(prev.transform.rotation) * offset; //calculates the inverse of the previous objects rotation, multiplied by the distance between them
+                offsets.Add(offset);                //adds it to the list of offsets
             }            
-            children.Add(current);
+            children.Add(current); //adds current object to list of children
         }
     }
 
@@ -32,16 +32,16 @@ public class SpineAnimator : MonoBehaviour {
         {
             Transform prev = children[i - 1];
             Transform current = children[i];
-            Vector3 wantedPosition = prev.position + ((prev.rotation * offsets[i-1]));
-            Quaternion wantedRotation = Quaternion.LookRotation(prev.transform.position - current.position, prev.transform.up);
+            Vector3 wantedPosition = prev.position + ((prev.rotation * offsets[i-1])); //previous objects position added to it's rotation multiplied by it's offset value
+            Quaternion wantedRotation = Quaternion.LookRotation(prev.transform.position - current.position, prev.transform.up); //calulates the rotationn needed to look at the previous object
 
-            Vector3 lerpedPosition = Vector3.Lerp(current.position, wantedPosition, Time.deltaTime * damping);
-            Vector3 clampedOffset = lerpedPosition - prev.position;
-            clampedOffset = Vector3.ClampMagnitude(clampedOffset, offsets[i-1].magnitude);
-            current.position = prev.position + clampedOffset;
+            Vector3 lerpedPosition = Vector3.Lerp(current.position, wantedPosition, Time.deltaTime * damping); //lerps between current pos and wanted pos, speed affected by damping
+            Vector3 clampedOffset = lerpedPosition - prev.position; //calculate distance between segments
+            clampedOffset = Vector3.ClampMagnitude(clampedOffset, offsets[i-1].magnitude); //clamp max distance between segments
+            current.position = prev.position + clampedOffset; //keep segments within max distance
 
 
-            current.rotation = Quaternion.Slerp(current.rotation, wantedRotation, Time.deltaTime * damping);
+            current.rotation = Quaternion.Slerp(current.rotation, wantedRotation, Time.deltaTime * damping); //rotates segments appropriately
         }
     }
 }
--- Game Engines Examples 2020/Assets/TentacleGenerator1.cs
+++ Game Engines Examples 2020/Assets/TentacleGenerator1.cs
@@ -12,16 +12,16 @@ public class TentacleGenerator1 : MonoBehaviour {
     void Awake () {
         for (int i = 0; i < numSegments; i++)
         {
-            Vector3 pos = -i * Vector3.forward * 1.1f;
-            GameObject prefab = (i == 0) ? headPrefab : segmentPrefab;
-            GameObject segment = GameObject.Instantiate<GameObject>(prefab);
+            Vector3 pos = -i * Vector3.forward * 1.1f; //creates a vector, scaling with i
+            GameObject prefab = (i == 0) ? headPrefab : segmentPrefab; //chooses which prefab to use
+            GameObject segment = GameObject.Instantiate<GameObject>(prefab); //instantiates the chosen prefab
 
-            segment.transform.position = (transform.rotation * pos) + transform.position;
+            segment.transform.position = (transform.rotation * pos) + transform.position; //moves the new segment into place
             //segment.transform.position = // transform.TransformPoint(pos);
-            segment.transform.rotation = transform.rotation;
-            segment.transform.parent = this.transform;
+            segment.transform.rotation = transform.rotation; //matches new segments rotation to this one
+            segment.transform.parent = this.transform; //sets this segment as new segments parent
             segment.GetComponent<Renderer>().material.color =
-                Color.HSVToRGB(i / (float)numSegments, 1, 1);
+                Color.HSVToRGB(i / (float)numSegments, 1, 1); //set color of new segment, based on number of segments
         }
 	}
 	
